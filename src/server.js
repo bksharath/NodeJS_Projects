e16e172ab35f056390ca2512b0e0cc5608d07541
sharath <shthbk@gmail.com>
2021-05-30 22:37:06 +0530

    POST Endpoint suppport

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import {promises as fs} from 'fs';
+import bodyParser from 'body-parser';
 
 let app = express();
+app.use(bodyParser.json());
 
 app.listen(3001,()=>{
     console.log("Server is listenning on port 3001");
@@ -21,6 +23,24 @@ app.get('/books/:id',async (req,res)=>{
     res.json(book);
 
 });
+
+app.post('/books',async (req,res)=>{
+   let newBook = req.body;
+
+   var data = await fs.readFile(__dirname + '/books.json');
+
+   var newData = JSON.parse(data);
+    
+   newData.push(newBook);
+
+   var stringData = JSON.stringify(newData);
+   
+   await fs.writeFile(__dirname + '/books.json',stringData);
+   res.json(newData);
+
+
+});      
+
 
 app.get('/books-fs',async (req,res)=>{
       let data = await fs.readFile(__dirname + '/books.json');
